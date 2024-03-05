edd2d2a3e10e963258906c862d662c5140bdc8f1
gykim93 <gykim1993@gmail.com>
2024-03-05 10:17:18 +0900

    loog example

--- src/day2/Loop.java
+++ src/day2/Loop.java
@@ -22,6 +22,35 @@ public class Loop {
             System.out.println("실행");
         }
 
+        // 1부터 10까지 출력
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        // 1부터 10까지 짝수 출력 -v1
+        for(int i = 2; i <= 10; i+=2) {
+            System.out.println(i);
+        }
+
+        // 1부터 10까지 짝수 출력 -v2
+        for(int i = 1; i <= 10; i++) {
+            if(i % 2 == 0) {
+                System.out.println(i); // 짝수일 때만 실행
+            }
+        }
+
+        // 2의 배수 == 짝수
+        // 2의 배수는 2로 나누어 떨어진다 -> 나머지가 0이다.
+        // 나머지 구하는 연산자 %
+
+
+
+
+        // 1부터 10까지 4의 배수 출력
+//        for(int i = 4; i <= 10; i+=4) {
+//            System.out.println(i);
+//        }
+
 
 
 
