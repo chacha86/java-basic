be605e8990306aa91f460aabce5066f7161b41fc
gykim93 <gykim1993@gmail.com>
2024-03-06 10:32:19 +0900

    method

--- src/day3/Reference.java
+++ src/day3/Reference.java
@@ -28,14 +28,27 @@ public class Reference {
         System.out.println(p1.name);
 
         // 손흥민을 2명 만들어주세요.(단 코드의 중복은 없어야 합니다.)
+        // 손흥민, 31, 런던
+
+        Person p4 = new Person();
+        p4.name = "손흥민";
+        p4.age = 31;
+        p4.home = "런던";
+
+
+        Person p5 = new Person();
+        p5.name = p4.name;
+        p5.age = p4.age;
+        p5.home = p4.home;
+
+        p4.name = "손흥만";
+        System.out.println(p4.name);
+        System.out.println(p5.name);
+
+        // 객체의 복사
+        // 참조값의 복사 -> 얕은 복사
+        // 객체값의 복사 -> 깊은 복사
 
 
     }
 }
-
-class Person {
-    String name;
-    int age;
-    String home;
-}
-
