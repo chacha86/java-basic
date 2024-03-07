768fa1ff689992420343a4e47db909c19f8414db
gykim93 <gykim1993@gmail.com>
2024-03-07 11:32:13 +0900

    duck exam1 solve

---
+++ src/day4/duckSimul/DuckSimulation.java
@@ -0,0 +1,15 @@
+package day4.duckSimul;
+
+public class DuckSimulation {
+    public static void main(String[] args) {
+        오리 a오리 = new 오리();
+        a오리.날다();
+        // 출력 : 오리가 날개로 날아갑니다.
+    }
+}
+
+class 오리 {
+    public void 날다() {
+        System.out.println("오리가 날개로 날아갑니다.");
+    }
+}
