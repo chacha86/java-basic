219ce3d67cebd8bef3ea7ffbcd89e59bb234c153
gykim93 <gykim1993@gmail.com>
2024-03-07 09:52:26 +0900

    account deposit solve

---
+++ src/day4/Account.java
@@ -0,0 +1,12 @@
+package day4;
+
+public class Account {
+
+    int remainder = 0;
+    public void deposit(int depositMoney) {
+        remainder += depositMoney;
+        System.out.println(depositMoney+ "원을 예금했습니다.");
+        System.out.println("잔액: " + remainder + "원");
+
+    }
+}
